914701f541774981719288963b88ad759b358188
jogean <jogeanmcf@gmail.com>
2021-09-21 22:50:15 -0300

    new commit

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div> Olá Mundo</div>
+    <div>
+      <h1> This is a test</h1>
+      <p> Lorem ipson</p>
+    </div>
   )
 }
