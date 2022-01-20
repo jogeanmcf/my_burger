fd0beaccdbbf1b5d395dfd5fb50fe3c3603c2c30
Jogean <jogeanmcf@gmail.com>
2022-01-20 19:05:43 -0300

    ui update

--- components/LeftNavbar.js
+++ components/LeftNavbar.js
@@ -15,7 +15,7 @@ const LeftNavbar = () => {
             <div>
                 <h1>Cansado de ficar esperdo por sua refeição?</h1>
                 <p>Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a crescente influência da mídia obstaculiza a apreciação da importância das posturas dos órgãos dirigentes com relação às suas atribuições.</p>
-                <button>Faça seu pedido</button>
+                <button>Faça seu pedido   &#8594;</button>
             </div>
         </div>
     );
--- components/RightNavbar.js
+++ components/RightNavbar.js
@@ -1,9 +1,97 @@
 import styles from '../styles/Home.module.css'
-import Image from 'next/image';
+import React, { Component } from 'react';
+import Carousel from "react-simply-carousel";
+import db from '../shared/db.json'  
 
-const Promotions = () => {
-    
+const PromoCard = ({promoItem}) => {
+    return (
+        <div className={styles.promoCard} key={promoItem.id.toString()} style={{width:200}} >
+            <h1>{promoItem.name}</h1>
+            {/* <p>{promoItem.about}</p> */}
+            <h4>{promoItem.price}</h4>
+            <h3> por {promoItem.newPrice}</h3>
+        </div>
+    );
 }
+
+class Promotions extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            activeSlideIndex: 0,
+            promotions: db.promotions,
+        }
+    }
+
+    setActiveSlideIndex = (newActiveSlideIndex) => {
+        this.setState({
+          activeSlideIndex: newActiveSlideIndex%3,
+        });
+      };
+
+    render(){
+        const list = ["oi", "tudo ", "bem ", "com ", "vc" ]
+
+        return(
+            <div className={styles.promotions}>
+                <Carousel
+                    updateOnItemClick
+                    containerProps={{
+                        style: {
+                          width: "100%",
+                          justifyContent: "space-between"
+                        }
+                      }}
+                    activeSlideIndex={this.state.activeSlideIndex}
+                    onRequestChange={this.setActiveSlideIndex}
+                    itemsToShow={2}
+                    itemsToScroll={1}
+                    easing="linear"
+                    forwardBtnProps={{
+                        children:"❯",
+                        style: {
+                            width: 20,
+                            height: 60,
+                            minWidth: 60,
+                            alignSelf: "center",
+                            color: "white",
+                            backgroundColor: "transparent",
+                            border: "none",
+                            fontSize: "20px",
+                            fontWeight: "bold"
+                        }
+                          }
+                    }
+                    backwardBtnProps={{
+                            children:"❰",
+                            style: {
+                                width: 20,
+                                height: 60,
+                                minWidth: 60,
+                                alignSelf: "center",
+                                color: "white",
+                                backgroundColor: "transparent",
+                                border: "none",
+                                fontSize: "20px",
+                                fontWeight: "bold"
+                            }
+                              }
+                    }
+                    speed={600}
+
+                >
+                    
+                    
+                    {this.state.promotions.map((item) => <PromoCard promoItem={item}/>)}
+                
+                </Carousel>
+            </div>
+        );
+    }
+}
+
+
 
 const RightNavbar = () => {
     return (
@@ -13,7 +101,9 @@ const RightNavbar = () => {
                 <button className={styles.btn2}>Pesquisar</button>
                 <button className={styles.btn1}>Login</button>
             </nav>
-
+            <div className={styles.promotions}>
+                <Promotions/>
+            </div>
         </div>
         
     );
